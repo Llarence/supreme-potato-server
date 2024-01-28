c5b9869549c242658f5b3c21bf1f7c885b43f2b4
Llarence <1010kai.t1010@gmail.com>
2024-01-27 19:42:14 -0800

    refactor frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
+    reactStrictMode: true,
+    async rewrites() {
         return [
             {
                 source: '/api/:path*',
